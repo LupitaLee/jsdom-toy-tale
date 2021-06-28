d5dba95686070f1f511fa33187a93c928cdf970c
Lupita <lupitarivera8899@gmail.com>
2021-06-28 15:45:54 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 let addToy = false;
 
+const toyCollect = document.querySelector('#toy-collection')
+// const card = 
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -12,4 +16,25 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+
+  getToys()
+
 });
+
+function getToys(){
+  return fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(json => console.log(json))
+}
+
+function postToys(){
+  return fetch('http://localhost:3000/toys',{
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+    },
+    body: JSON.stringify()
+  })
+}
