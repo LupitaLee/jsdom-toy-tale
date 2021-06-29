a74d29726ff9affca9b1366271eb8df696cc4a97
Lupita <lupitarivera8899@gmail.com>
2021-06-28 19:03:00 -0500

    commit tree e217fe56cb42a076357a44a754183ee6c4cf80d4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 let addToy = false;
 
-const toyCollect = document.querySelector('#toy-collection')
-// const card = 
+const divCollect = document.querySelector('#toy-collection')
 
 
+  
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -17,24 +17,87 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-
-  getToys()
-
 });
 
 function getToys(){
   return fetch('http://localhost:3000/toys')
   .then(resp => resp.json())
-  .then(json => console.log(json))
+  
+  
+  
+}
+function postToys(toys){
+  return fetch('http://localhost:3000/toys', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    "Accept": "application/json"
+  }, 
+  body: JSON.stringify({
+    name: toys.name.value,
+    image: toys.image.value,
+    likes: toys.likes.value
+  })
+  .then(resp => resp.json())
+  .then((toy) => {
+    renderToys(toy)
+  })
+})};
+
+
+// function likes(e) {
+//   e.preventDefault()
+//   let more = parseInt(e.target.previousElementChild.innerText) + 1
+//   fetch(`http://localhost:3000/toys/${e.target.id}`, {
+//       method: "PATCH",
+//       headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+
+//       },
+//       body: JSON.stringify({
+//         "likes": more
+//       })
+//     })
+//     .then(res => res.json())
+//     .then((toy => {
+//       e.target.previousElementSibling.innerText = `${more} likes`;
+//     }))
+// }
+
+
+
+function renderToys(toy) {
+  let h2 = document.createElement('h2')
+  h2.innerText = toy.name
+
+  let img = document.createElement('img')
+  img.setAttribute('src', toy.image)
+  img.setAttribute('class', 'toy-avatar')
+
+  let p = document.createElement('p')
+  p.innerText = `${toy.likes} likes`
+
+  let btn = document.createElement('button')
+  btn.setAttribute('class', 'like-btn')
+  btn.setAttribute('id', toy.id)
+  btn.innerText = "like"
+  btn.addEventListener('click', (e) => {
+    console.log(e.target.dataset);
+    likes(e)
+  })
+
+  let divCard = document.createElement('div')
+  divCard.setAttribute('class', 'card')
+  divCard.append(h2, img, p, btn)
+  divCollect.append(divCard)
 }
 
-function postToys(){
-  return fetch('http://localhost:3000/toys',{
-    method: "POST",
-    headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-    },
-    body: JSON.stringify()
+// start by getting all toys
+
+getToys().then(toys => {
+  toys.forEach(toy => {
+    //function to render toys goes here or something
+    renderToys(toy)
   })
-}
+})
